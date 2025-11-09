7c9f82f6c4a27d09bfc4864687949c832bad0635
Shashi Kiran <42802744+shashikiran1491@users.noreply.github.com>
2025-11-09 19:26:18 +0530

    fix issues (#24)
    Co-authored-by: Shashi Kiran <shashi@Shashis-MacBook-Pro-2.local>

--- expense-tracker-service/src/main/java/com/java/expense/service/SummaryService.java
+++ expense-tracker-service/src/main/java/com/java/expense/service/SummaryService.java
@@ -1,6 +1,7 @@
 package com.java.expense.service;
 
 import com.java.expense.entity.User;
+import com.java.expense.enums.Category;
 import com.java.expense.model.CategorySummary;
 import com.java.expense.model.CategorySummaryResponse;
 import com.java.expense.model.SummaryResponse;
@@ -14,7 +15,9 @@ import java.math.RoundingMode;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -30,8 +33,8 @@ public class SummaryService {
         LocalDateTime endDateTime = endDate.atTime(23, 59, 59);
 
         User user = userService.getUser(userEmail);
-        double totalIncome = expenseRepository.sumByTypeAndDateRange(user,"Income", startDateTime, endDateTime);
-        double totalExpense = expenseRepository.sumByTypeAndDateRange(user,"Expense", startDateTime, endDateTime);
+        double totalIncome = expenseRepository.sumByTypeAndDateRange(user, "Income", startDateTime, endDateTime);
+        double totalExpense = expenseRepository.sumByTypeAndDateRange(user, "Expense", startDateTime, endDateTime);
         double netBalance = totalIncome - totalExpense;
 
         return SummaryResponse.builder()
@@ -46,24 +49,47 @@ public class SummaryService {
         LocalDateTime endDateTime = endDate.atTime(23, 59, 59);
 
         User user = userService.getUser(userEmail);
+
+        List<String> categories = Category.getAllCategories();
+
         List<CategorySummary> categorySummaries = expenseRepository.findCategoryWiseSummary(user, startDateTime, endDateTime);
+        Map<String, Double> categoryExpenseMap = categorySummaries
+                .stream()
+                .collect(Collectors.toMap(
+                        CategorySummary::getCategory,
+                        CategorySummary::getTotalAmount
+                ));
 
         double totalExpense = categorySummaries.stream()
-               .mapToDouble(CategorySummary::getTotalAmount)
-               .sum();
+                .mapToDouble(CategorySummary::getTotalAmount)
+                .sum();
 
-        return categorySummaries.stream()
-                .map(cs -> CategorySummaryResponse
-                        .builder()
-                        .category(cs.getCategory())
-                        .totalAmount(cs.getTotalAmount())
-                        .percentage(calculatePercentage(cs, totalExpense))
-                        .build())
-                .toList();
+        List<CategorySummaryResponse> categorySummaryResponses =  categories.stream().map(category -> {
+                    double categoryExpense = getCategoryExpense(category, categoryExpenseMap);
+                    return toCategoryResponse(category, categoryExpense, totalExpense);
+                }).collect(Collectors.toList());
+
+        categorySummaryResponses.sort(Comparator.comparingDouble(CategorySummaryResponse::getPercentage).reversed());
+
+        return categorySummaryResponses;
+
     }
 
-    private static double calculatePercentage(CategorySummary cs, double totalExpense) {
-        return totalExpense > 0 ? BigDecimal.valueOf(100 * cs.getTotalAmount() / totalExpense)
+    private CategorySummaryResponse toCategoryResponse(String category, double categoryExpense, double totalExpense) {
+        return CategorySummaryResponse
+                .builder()
+                .category(category)
+                .totalAmount(categoryExpense)
+                .percentage(calculatePercentage(categoryExpense, totalExpense))
+                .build();
+    }
+
+    private static double getCategoryExpense(String category, Map<String, Double> categoryExpenseMap) {
+        return categoryExpenseMap.getOrDefault(category, 0.00);
+    }
+
+    private double calculatePercentage(double categoryExpense, double totalExpenses) {
+        return totalExpenses > 0 ? BigDecimal.valueOf(100 * categoryExpense / totalExpenses)
                 .setScale(2, RoundingMode.HALF_UP).doubleValue() : 0;
     }
 }
